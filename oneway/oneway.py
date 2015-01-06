50e5baf94d19be782d6eb12b4a6d3c99d6f87c94
Snuggert <abe.wiersma@hotmail.nl>
2015-01-06 13:47:16 +0100

    testcase

--- oneway/oneway.py
+++ oneway/oneway.py
@@ -6,19 +6,19 @@ from world import World
 def main():
     myWorld = World(1000)
 
-    myWorld.add_car(2, 2, 100)
+    myWorld.add_car(2, 0, 2)
     for i in range(1):
-        myWorld.add_car(2, i * 10)
+        myWorld.add_car(2, 0, i * 10)
 
     env = simpy.Environment()
     env.process(simulate(env, myWorld, 1))
-    env.run(until=1000)
+    env.run(until=10)
     plt.show()
     
 
 def simulate(env, world, tick):
     while True:
-        world.plot_cars(tick)
+        world.move_cars(tick)
         print("time:", env.now)
         yield env.timeout(tick)
 
