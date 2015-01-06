656b8b212d49e4f14c551092eaf883c57e54f62a
chpbrijs <chpbrijs@gmail.com>
2015-01-06 07:54:31 -0500

    hoi

--- oneway/car.py
+++ oneway/car.py
@@ -1,19 +1,24 @@
 class Car:
-    next_speed = 0
-
     max_brake = -10
     max_accellerate = 2
     t_react = 1
 
     def __init__(self, speed, location):
-        self.speed = speed
+        self.speed = [speed, speed, ]
         self.location = location
+        self.next_speed = speed
+        
 
     def set_location(self, location):
         self.location = location
 
     def set_next_speed(self, new_speed):
-        self.next_speed = new_speed
+        self.speed.remove(self.speed[0])
+        self.speed.append(new_speed)
 
-    def apply_next_speed(self):
-        self.speed = self.next_speed
+    def accellerate(self, max_speed, timedelta):
+        new_speed = self.speed + self.max_accellerate * timedelta
+        if new_speed > max_speed:
+            new_speed = max_speed
+                    
+        self.set_next_speed(new_speed)
--- oneway/world.py
+++ oneway/world.py
@@ -20,22 +20,38 @@ class World:
             car = self.cars[0]
             
         for i, car in enumerate(self.cars):
-            new_location = timedelta * car.speed + car.location
+            print "auto %d begint op locatie", car.location, "met snelheid", \
+                  car.speed[0]
+            new_location = car.location + timedelta * 0.5 * (car.speed[0] + \
+                                                             car.speed[1])
+            print "rijdt naar ", new_location
             car.location = new_location
 
             if(i is not 0):
+                # find gap
                 relative_speed = car.speed - self.cars[i - 1].speed
                 t_brake = relative_speed / car.max_brake
-                x_brake = t_brake * (self.cars[i - 1].speed + 0.5 * relative_speed)
+                x_brake = t_brake * (self.cars[i - 1].speed + \
+                                     0.5 * relative_speed)   
                 gap = self.cars[i-1].location - car.location
+                if gap < 0:
+                    print "FATAL ERROR :( "
+
+                # decide to brake
                 if(gap <= x_brake):
+                    print "hij gaat straks remmen"
                     new_speed = car.speed - car.max_brake
                     if(new_speed < 0):
                         new_speed = 0
                     car.set_next_speed(new_speed)
+                else:
+                    print "hij gaat harder rijden"
+                    car.accellerate(max_speed, timedelta)
                     
             else:
-                car.set_next_speed(car.speed + car.max_accellerate)
+                car.accellerate(max_speed, timedelta)
+
+            print "auto %d nu op positie", car.location, 
 
     def plot_cars(self):
         cars_xy = [[], []]
